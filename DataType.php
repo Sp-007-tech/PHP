58ec30a69dae36b48ba0fe91ffcee7a80d85a302
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-15 23:21:37 +0530

    just added

---
+++ DataType.php
@@ -0,0 +1,37 @@
+<?php
+// Data type in php 
+//1.String 
+//2.Integer
+//3.Float
+//4.Boolean 
+//5.Array 
+//6.Object 
+
+
+echo " Data types in PHP <br><br>";
+
+$var0 = "This is a string";
+echo var_dump($var0);
+echo "<br>";
+
+$var1 = 69;
+echo var_dump($var1);
+echo "<br>";
+
+$var2 = 69.9;
+echo var_dump($var2);
+echo "<br>";
+
+$var3 = True;
+echo var_dump($var3);
+echo "<br> <br>";
+
+$cars = array("BMW","Tyoto","Buggati","volvo");
+echo "$cars[0]";echo "<br>";
+echo "$cars[1]";echo "<br>";
+echo "$cars[2]";echo "<br>";
+echo "$cars[3]";echo "<br>";
+
+
+
+?>
