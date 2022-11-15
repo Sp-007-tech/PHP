a6b16e4acf69c5070e34e306beae7922ad88d39b
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-15 23:23:44 +0530

    Add files via upload

---
+++ DATEfunc.php
@@ -0,0 +1,19 @@
+<?php
+
+echo " Today is " . date("Y/m/d") . "<br>"; 
+echo " today is " . date("Y.m.d") . "<br>";
+echo " Today is " . date("Y-m-d") . "<br>";
+echo " Today is " . date("Y-m-d") . "<br>";
+echo " Today is " . date("1") . "<br>";
+?>
+
+<?php
+echo "The time is " . date("h:i:sa") . "<br>";
+?>
+
+<?php
+    date_default_timezone_set("America/New_York");
+    echo "The time is " . date("h:i:sa");
+
+?>
+
