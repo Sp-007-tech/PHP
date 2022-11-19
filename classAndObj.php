5764b73c4815b257bef2ca1474f48ab90fbbf424
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-19 20:00:49 +0530

    added

---
+++ classAndObj.php
@@ -0,0 +1,32 @@
+<!-- A class is a template of objects, and an object is an instance of class. -->
+
+<!-- In a class, variable are called properties and function are called methods.. -->
+
+<?php
+
+    class Fruit {
+        // properties
+        public $name;
+        public $color;
+    
+
+    // Method
+    function set_name($name){
+        $this->$name = $name;
+    }
+
+    function get_name(){
+        return $this->name;
+    }
+}
+// object of class
+$apple = new Fruit();
+$mango = new Fruit();
+$apple->set_name('Apple');
+$mango->set_name('Mango');
+
+echo $apple->get_name();
+echo $mango->get_name();
+
+
+?>
