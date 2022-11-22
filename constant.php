11a0c480e4a30d50c3d159f42bb9c647b408674c
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-22 17:32:39 +0530

    uploaded !

---
+++ constant.php
@@ -0,0 +1,18 @@
+<!-- (1)Constant are like variable expect that once they are defined they cannot be changed 
+(2) A constant is an identifire or simply name for value.
+(3)The constant values cannot be changed during the script.
+(4)A valid constant name start with letter or underscore.
+(5)To create constant, we use "define()" fucntion.
+(6)SYNTAX::   define(name,value,case-insensitive)]
+(7) name: It specifies the name of constant.
+    value: It specifies the vlaue of constant.
+    case-insensitive: It specifies whether the constant name should be case-insensitive.
+-->
+
+
+<?php
+
+define("GREETING","Wlecome tho !");
+echo GREETING;
+
+?>
