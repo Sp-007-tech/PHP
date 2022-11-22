6215d5228d76f1f064ca1ae0a9e347913bfb02dc
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-22 17:34:41 +0530

    uploaded !

---
+++ includeFiles.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<h1>Welcome to my page! </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur quia facere dolores? Modi nisi velit rerum qui rem repellat tenetur optio ipsam incidunt. Ipsum neque id nulla pariatur vero.</p>
+
+<p>Some more text..</p>
+<?php include 'example.php';?>
+</body>
+</html>
