dc33f1cf829058893dcd31e20b2caff072abe639
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-01-01 17:34:24 +0530

    Updated !

--- includeFiles.php
+++ includeFiles.php
@@ -2,7 +2,7 @@
 <body>
 
 <h1>Welcome to my page! </h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur quia facere dolores? Modi nisi velit rerum qui rem repellat tenetur optio ipsam incidunt. Ipsum neque id nulla pariatur vero.</p>
+<p>Lorem ipsum dolor sit adipisicing elit. Iusto consequatur quia facere dolores? Modi nisi ipsam incidunt. Ipsum neque id nulla pariatur vero.</p>
 
 <p>Some more text..</p>
 <?php include 'example.php';?>
