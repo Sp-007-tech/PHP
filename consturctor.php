2ded01191392def883480d874586696f5fe92a05
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-01-01 17:22:28 +0530

    Updated !

--- consturctor.php
+++ consturctor.php
@@ -1,18 +1,22 @@
 <!--(1)A constructor allows you to initialize object's properties upon creation of object.   (2)If you create "__construct()" fuction, php will automatically call the function when you create an object for a class.  (3)Here construct function will start with the two underscores(__) -->
 
-
 <?php
 
 class Fruit {
     public $name;
-
+    
+    //constructor created     
     function __construct($name){
         $this->name=$name;
     }
+    
+    //function created     
     function get_name(){
         return $this->$name;
     }
 }
+
+// object created 
 $apple = new Fruit("Apple");
 echo $apple->get_name();
 
@@ -30,15 +34,19 @@ class fruit {
         $this->name=$name;
         $this->color=$color;
     }
+    
     function get_name(){
         return $this->name;
     }
+    
     function get_color(){
         return $this->color;
     }
 }
 
 $apple = new fruit("Apple","Red");
+
 echo $apple->get_name();
 echo $apple->get_color();
+
 ?>
